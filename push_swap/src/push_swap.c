34678121e4fdadc1c9e686a52f67cd0639f87662
kmularise <kmula222@gmail.com>
2023-03-01 18:54:31 +0900

    make stack

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -35,41 +35,51 @@ int check_right_cmd(char **argv)
 
 	i = 0;
 	while (argv[++i])
-		if (is_number(argv[i]))
+		if (!is_number(argv[i]))
 			return (0);
 	return (1);
 }
-void make_stack(t_stack *a, t_stack *b, char **argv)
+void make_stack(t_stack **a, t_stack **b, char **argv)
 {
 	int	i;
 	t_node *temp;
 
-	i = 0;
-	a = malloc(sizeof(t_stack));
-	b = malloc(sizeof(t_stack));
-	b->head = NULL;
-	b->size = 0;
-	a->head = create_node(argv[0]);
-	a->size = 0;
-	while (argv[i])
+	i = 1;
+	*a = malloc(sizeof(t_stack));
+	*b = malloc(sizeof(t_stack));
+	(*b)->head = NULL;
+	(*b)->size = 0;
+	(*a)->head = create_node(ft_atoi(argv[i]));
+	(*a)->size = 1;
+	while (argv[++i])
 	{
-		temp = create_node(argv[i]);
-		push_node(&(a->head), temp);
-		(a->size)++;
-		i++;
+		temp = create_node(ft_atoi(argv[i]));
+		push_node(&((*a)->head), temp);
+		(*a)->size += 1;
 	}
 
 }
+
+void show_error(char *message)
+{
+	ft_putstr_fd(message, 2);
+	exit(1);
+}
 int main(int argc, char **argv)
 {
-	t_stack a;
-	t_stack b;
-	if (check_right_cmd(argv))
-		make_stack(&a, &b, argv);
-	for (int i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+	t_stack *a;
+	t_stack *b;
+	if (argc == 1)
+		show_error("Error\n");
+	if (!check_right_cmd(argv))
+		show_error("Error\n");
+	make_stack(&a, &b, argv);
+	print_list_reverse(&(a->head));
+	// printf("size :%d\n",(a.head)->data);
+	// for (int i = 0; i < argc; i++)
+	// {
+	// 	printf("%s\n", argv[i]);
+	// }
 
 	
 }
--- push_swap/src/test.sh
+++ push_swap/src/test.sh
@@ -1,1 +1,2 @@
-cc -Wall -Wextra -Werror stack.c stack2.c main.c
+# cc -Wall -Wextra -Werror stack.c stack2.c main.c 
+cc -Wall -Wextra -Werror stack.c stack2.c push_swap.c ../libft/*.c
