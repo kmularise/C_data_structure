7d581ae5ca5e93943f90ce58f4bb127770a12245
Yuijin Kim <yuikim@c5r4s7.42seoul.kr>
2023-03-02 21:56:26 +0900

    index processing

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: yuikim <yuikim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 19:49:06 by yuikim            #+#    #+#             */
-/*   Updated: 2023/03/01 20:41:57 by yuikim           ###   ########.fr       */
+/*   Updated: 2023/03/02 21:54:44 by yuikim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ int	check_right_cmd(char **argv)
 }
 void	init_stack(t_stack **a, t_stack **b, char **argv)
 {
-	int	i;
+	int		i;
 	t_node	*temp;
 
 	i = 1;
@@ -65,28 +65,84 @@ void	show_error(char *message)
 	ft_putstr_fd(message, 2);
 	exit(1);
 }
+
+void quick_sort(int *list, int first, int last)
+{
+	int	left;
+	int	right;
+	int	pivot;
+	int	temp;
+
+	left = first;
+	right = last;
+	pivot = list[(first + last) / 2];
+	while (1)
+	{
+		while (list[left] < pivot)
+			left++;
+		while (list[right] > pivot)
+			right--;
+		if (left <= right)
+		{
+			temp = list[left];
+			list[left] = list[right];
+			list[right] = temp;
+			left++;
+			right--;
+		}
+		if (left > right)
+			break ;
+	}
+	if (right > first)
+		quick_sort(list, first, right);
+	if (left < last)
+		quick_sort(list, left, last);
+}
+
+void	sort(int **sorted_list, int count, char **argv)
+{
+	int	i;
+	(*sorted_list) = malloc(sizeof(int) * count);
+	
+	i = 1;
+	while (argv[i])
+	{
+		(*sorted_list)[i - 1] = ft_atoi(argv[i]);
+		printf("%d %d \n", i, ft_atoi(argv[i]));
+		i++;
+	}
+	quick_sort((*sorted_list), 0, count -1);
+	for (i=0; i < count; i++){
+		printf("%d ", (*sorted_list)[i]);
+	}
+}
+
 int main(int argc, char **argv)
 {
-	t_stack *a;
-	t_stack *b;
+	// t_stack *a;
+	// t_stack *b;
+	int	*sorted_list;
+
 	if (argc == 1)
 		show_error("Error\n");
 	if (!check_right_cmd(argv))
 		show_error("Error\n");
-	init_stack(&a, &b, argv);
-	print_list_reverse(&(a->top));
-	execute_s(&a);
-	print_list_reverse(&(a->top));
-	execute_p(&b, &a);
-	print_list_reverse(&(a->top));
-	print_list_reverse(&(b->top));
-	execute_rr(&a);
-	print_list_reverse(&(a->top));
-	// printf("size :%d\n",(a.head)->data);
-	// for (int i = 0; i < argc; i++)
-	// {
-	// 	printf("%s\n", argv[i]);
-	// }
+	sort(&sorted_list, argc - 1, argv);
+	//min ~ pivot1 설정
+	//pivot1 ~ pivot2 설정 가능
+	//pivot2 ~max 설정 가능
 
-	system("leaks a.out");
+
+
+	// init_stack(&a, &b, argv);
+	// print_list_reverse(&(a->top));
+	// execute_s(&a);
+	// print_list_reverse(&(a->top));
+	// execute_p(&b, &a);
+	// print_list_reverse(&(a->top));
+	// print_list_reverse(&(b->top));
+	// execute_rr(&a);
+	// print_list_reverse(&(a->top));
+
+	// system("leaks a.out");
 }
